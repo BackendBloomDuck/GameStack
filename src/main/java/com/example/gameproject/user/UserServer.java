95de0381d455871a78fd79070473683247c84b7f
YasirAlghamdi <yasir.alghamdi@bloomduck.co>
2023-07-05 11:08:12 +0300

    GG

--- src/main/java/com/example/gameproject/user/UserServer.java
+++
@@ -1,7 +0,0 @@
-package com.example.gameproject.user;
-
-import com.example.gameproject.exception.UserNotFoundException;
-
-public interface UserServer {
-    User getUserById(int id) throws UserNotFoundException;
-}
